3bfc04a2ef864948f743d8d73e485e3f9458aff3
Vybhav Achar Bhargav <vacharbh@ucsc.edu>
2019-07-24 01:38:14 -0700

    ADding display images

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,44 @@ import React from 'react';
 
 export default () => (
   <div className="container">
-    <h1 className="is-size-1">Clean house, clean mind!</h1>
-    <text className="label">vania's display pic goes under here</text>
+
+    <div className="columns is-vcentered is-multiline">
+      <div className="column is-half">
+        <figure className="image is-square">
+          <img src="https://i.ibb.co/FYtYDNz/IMG-0197.png" />
+        </figure>
+      </div>
+      <div className="column is-half">
+        <h1 className="title has-text-link is-size-1">CREATE CHORES</h1>
+        <p className="subtitle is-size-4 has-text-link">With Choreganizer, you can create multiple chores which you can assign to respective housemates!</p>
+      </div>
+      <div className="column is-half">
+        <h1 className="title has-text-link is-size-1 has-text-right">HOLD PEOPLE ACCOUNTABLE</h1>
+        <p className="subtitle is-size-4 has-text-link has-text-right"><i>Choreganizer</i> holds your housemates accountable for the chores they are assigned</p>
+      </div>
+      <div className="column is-half">
+        <figure className="image is-square">
+          <img src="https://i.ibb.co/QrB0D8J/IMG-0198.png" />
+        </figure>
+      </div>
+      <div className="column is-half">
+        <figure className="image is-square">
+          <img src="https://i.ibb.co/vQWLBwd/IMG-0199.png" />
+        </figure>
+      </div>
+      <div className="column is-half">
+        <h1 className="title has-text-link is-size-1">CLEAN HOUSE, CLEAN MIND</h1>
+        <p className="subtitle is-size-4 has-text-link"><i>Choreganizer</i> helps you and your housemates maintain a
+        a squeaky clean home without the stress!</p>
+      </div>
+    </div>
+
+    <div className="columns is-vcentered">
+
+    </div>
+
+    <div className="columns is-vcentered">
+
+    </div>
   </div>
 );
